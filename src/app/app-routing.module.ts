9895c0fbc6bd111e14424cbbb28aa224e290e7d2
jaydiprakholiya <jaydiprakholiya1611@gmail.com>
2021-05-22 17:41:56 +0530

    as

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import{NgModule}from'@angular/core';
 import{Routes, RouterModule}from'@angular/router';
+import { BlogComponent } from './blog/blog.component';
 import { CapsulesComponent } from './capsules/capsules.component';
+import { MissionComponent } from './mission/mission.component';
 import { RocketsComponent } from './rockets/rockets.component';
 
 
 const routes:Routes = [
     {path:'capsules',component:CapsulesComponent},
-    {path:'Rockets',component:RocketsComponent}
+    {path:'Rockets',component:RocketsComponent},
+    {path:'Mission',component:MissionComponent},
+    {path:'Blog',component:BlogComponent}
 ];
 
 @NgModule({
@@ -15,4 +19,4 @@ exports:[RouterModule]
 })
 export class AppRoutingModules{ } 
 
-export const routingcomponent = [CapsulesComponent,RocketsComponent]
+export const routingcomponent = [CapsulesComponent,RocketsComponent,BlogComponent,MissionComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
 import { HeaderComponent } from './header/header.component';
 import {footercomponent} from './footer.component';
 import { NevigationComponent } from './nevigation/nevigation.component';
 import { AppRoutingModules,routingcomponent } from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { AppRoutingModules,routingcomponent } from "./app-routing.module";
     HeaderComponent,
     footercomponent,
     NevigationComponent,
-    routingcomponent  
+    routingcomponent,
+     
        
       
   ],
